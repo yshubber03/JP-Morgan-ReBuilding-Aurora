7cb422034b588514c622e06208daff46e92f8c09
Allie <ayang7@uchicago.edu>
2023-10-06 18:56:55 -0500

    added registration div element

--- frontendaurora/src/signin.js
+++ frontendaurora/src/signin.js
@@ -1,6 +1,6 @@
 import './signin.css'
 import axios from 'axios'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 
 /* there are some console errors about useNavigate, but it's bc we don't have a router yet*/
@@ -12,21 +12,25 @@ export default function SignUp(props){
     //const navigate = useNavigate()
   
     // state that stores values in inputs
-    const [inputValue, setInputValue] = useState({name: "", email:"", password: ""})
+    const [reginputValue, regsetInputValue] = useState({name: "", email:"", password: ""})
+    const [signinputValue, signsetInputValue] = useState({email:"", password: ""})
   
     // regex test to see if email is valid
-    const validEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(inputValue.email) || inputValue.email==""
+    const regvalidEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(reginputValue.email) || reginputValue.email==""
+    const signvalidEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(signinputValue.email) || signinputValue.email==""
   
     // returns an error if validEmail is false
-    const validEmailMessage = validEmail ? "": <div style={{textAlign:"left", padding: "0", color: "red", fontSize:".8rem"}}>*Please enter a valid email</div>
+    const regvalidEmailMessage = regvalidEmail ? "": <div style={{textAlign:"left", padding: "0", color: "red", fontSize:".8rem"}}>*Please enter a valid email</div>
+    const signvalidEmailMessage = signvalidEmail ? "": <div style={{textAlign:"left", padding: "0", color: "red", fontSize:".8rem"}}>*Please enter a valid email</div>
   
     // regex test to see if name is valid
-    const validName = /[^\d\s]/.test(inputValue.name) || inputValue.name==""
+    const regvalidName = /[^\d\s]/.test(reginputValue.name) || reginputValue.name==""
+    const signvalidName = /[^\d\s]/.test(signinputValue.name) || signinputValue.name==""
   
     // on submission, the function sends post request to backend, and navigates away if successful
+    //will need to have two copies of this function for register form and sign in form
     function handleSubmit(event){
       event.preventDefault();
-      console.log(inputValue.name)
       //will uncomment once we know where to post, missing the post url and success url 
       /* axios.post("", inputValue)
         .then(response => {
@@ -35,6 +39,17 @@ export default function SignUp(props){
         })
         .catch(error => console.log(error)) */
     }
+
+    useEffect(() => {
+        // makes the signin div disappear
+        var signin = document.getElementById('signin')
+        var register = document.getElementById('register')
+        signin.onclick = function() {
+            signin.style.display = 'none';
+            register.style.display = 'block';
+        }
+        
+      })
 
     //onclick register pulls up register div and disappears sign up div
     //function to appear register div, function to disappear sign up div
@@ -46,9 +61,35 @@ export default function SignUp(props){
                 <h1 id="" className="half-width">
                     Sign in
                 </h1>
-                <button id="register" className="half-width" type="button">
+                <button id="registerbutton" className="half-width" type="button">
                     Register
                 </button>
+            </div>
+
+            <div>
+                <form onSubmit={handleSubmit}>
+                    {/* email */}
+                    <div className="inputs">
+                        <strong>Email</strong>
+                        <input type="text" value={signinputValue.email} onChange={(e) => signsetInputValue({...signinputValue, email: e.target.value})}/>
+                        {signvalidEmailMessage}
+                    </div>
+
+                    {/* password */}
+                    <div className="inputs">
+                        <strong>Password</strong>
+                        <input type="text" value={signinputValue.password} onChange={(e) => signsetInputValue({...signinputValue, password: e.target.value})}/>
+                    </div>
+
+                    <input className="submitButton" type="submit"></input>
+                </form>
+            </div>
+        </div>
+
+        <div id="register">
+            <div>
+                <h2> Create your account </h2>
+                <p> Registration is easy. </p>
             </div>
 
             <div>
@@ -56,23 +97,23 @@ export default function SignUp(props){
                     {/* name */}
                     <div className="inputs">
                         <strong>Name</strong>
-                        <input type="text" value={inputValue.name} onChange={(e) => setInputValue({...inputValue, name: e.target.value})}/>
+                        <input type="text" value={reginputValue.name} onChange={(e) => regsetInputValue({...reginputValue, name: e.target.value})}/>
                     </div>
                     
                     {/* email */}
                     <div className="inputs">
                         <strong>Email</strong>
-                        <input type="text" value={inputValue.email} onChange={(e) => setInputValue({...inputValue, email: e.target.value})}/>
-                        {validEmailMessage}
+                        <input type="text" value={reginputValue.email} onChange={(e) => regsetInputValue({...reginputValue, email: e.target.value})}/>
+                        {regvalidEmailMessage}
                     </div>
 
                     {/* password */}
                     <div className="inputs">
                         <strong>Password</strong>
-                        <input type="text" value={inputValue.password} onChange={(e) => setInputValue({...inputValue, password: e.target.value})}/>
+                        <input type="text" value={reginputValue.password} onChange={(e) => regsetInputValue({...reginputValue, password: e.target.value})}/>
                     </div>
 
-                    <input className="submitButton" type="submit"></input>
+                    <input className="submitButton" type="submit" value="Register"></input>
                 </form>
             </div>
         </div>
