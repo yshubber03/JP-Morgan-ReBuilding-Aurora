9a7c248344f94b815b59ba18d9d06d999c786ab6
Charles Harris <Charlesharris2322@gmail.com>
2023-10-07 09:32:33 -0500

    Removed unused inports

--- frontendaurora/src/App.js
+++ frontendaurora/src/App.js
@@ -1,26 +1,14 @@
-// import UserPage from "./User/UserPage";
-// import 'font-awesome/css/font-awesome.min.css';
 import { AuthProvider } from './contexts/AuthContext';
-import { EventsProvider } from './contexts/calendar_context';
 import SignUp from "./signin";
 import _Navbar from "./_Navbar";
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {useState, useEffect} from 'react';
 import UserPage from './User/UserPage';
-//import Footer from './Footer';
-import {useAuth} from './contexts/AuthContext'
-import CalendarComponent from './landing/calender.js'
-import Gallery from './landing/gallery'
+import CalendarComponent from './Landing/calender.js'
+import Gallery from './Landing/gallery'
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Resources from './/resources/resources.js'
 
-
-///
-
-//name: a
-//email: a@a.aa
-//password: aaaaaa
 function App() {
   return (
     <AuthProvider>
