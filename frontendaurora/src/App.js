e6f1a0304d6719e33ad8fd2bb55df911e8ac4184
Allie <ayang7@uchicago.edu>
2023-10-06 19:28:34 -0500

    navbar and router with placeholder pages

--- frontendaurora/src/App.js
+++ frontendaurora/src/App.js
@@ -1,10 +1,21 @@
-
-import './App.css';
+import _Navbar from "./_Navbar";
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {useState, useEffect} from 'react';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <_Navbar />
+          <Routes>
+            <Route exact path="/" element={<div>hello home</div>}></Route>
+            <Route exact path="/2" element={<div>hello 2</div>}></Route>
+            <Route exact path="/3" element={<div>hello 3</div>}></Route>
+            <Route exact path="/4" element={<div>hello 4</div>}></Route>
+            <Route exact path="/5" element={<div>hello 5</div>}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontendaurora/src/_Navbar.js
@@ -0,0 +1,42 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useNavigate} from 'react-router-dom';
+import './_Navbar.css'
+export default function _Navbar(props){
+const navigate = useNavigate();
+
+
+  // bootstrap code for navbar
+  // everything in Navbar.Collapse tag is collapsible
+  return(
+    <Navbar bg="white" expand="sm" className="shadow-sm _navbar">
+      <Container fluid>
+        <Navbar.Brand style={{cursor: "pointer"}} className="" onClick={()=>{navigate("/")}}>
+        <img src="http://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1581533791977-LXZWE6Q3JU2GPGEGK0QQ/RTA+logo+2.JPG?format=1500w" id="logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link onClick={()=>{navigate("/2")}} className="text-dark">
+              <img style={{marginBottom: "5px"}} width="20px" className="navIcon me-1"></img>
+              2
+            </Nav.Link>
+            <Nav.Link onClick={()=>{navigate("/3")}} className="text-dark">
+              3
+            </Nav.Link>
+            <Nav.Link onClick={()=>{navigate("/4")}} className="text-dark">
+              4
+            </Nav.Link>
+            <Nav.Link onClick={()=>{navigate("/5")}} className="text-dark">
+              5
+            </Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+  )
+}
