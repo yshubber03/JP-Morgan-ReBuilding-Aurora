727695a59a6e056c969b5c93b93e1c2eb027d932
kellyzhangg <kellyzhangg12@gmail.com>
2023-10-06 22:30:45 -0500

    fixing dropdown - added state for toggled

--- frontendaurora/src/resources/ResourceCard.js
+++ frontendaurora/src/resources/ResourceCard.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 import './resources.css';
 
-const ResourceCard = ({ data, index }) => {
+const ResourceCard = ({ data }) => {
+  const [showCollapse, setShowCollapse] = useState(false);
+
+  const toggleCollapse = () => {
+    setShowCollapse(!showCollapse);
+  };
+
   return (
-    <Card style={{ width: '18rem' }} className = "resource-card">
+    <Card style={{ width: '18rem' }} className="resource-card">
       <Card.Img className="card-image" src={data.imageUrl} alt="Card image" />
       <Card.Body>
-        <Card.Title >{data.title}</Card.Title>
-        <Card.Text>
-          {data.description}
-        </Card.Text>
+        <Card.Title>{data.title}</Card.Title>
+        <Card.Text>{data.description}</Card.Text>
       </Card.Body>
       <Card.Body>
-        <Card.Link href={data.link}>Video </Card.Link>
+        <Card.Link href={data.link}>Video</Card.Link>
+        <button
+          className="btn btn-link"
+          type="button"
+          onClick={toggleCollapse}
+          aria-expanded={showCollapse}
+        >
+          Toggle Collapse
+        </button>
+        <div className={`collapse ${showCollapse ? 'show' : ''}`}>
+          <div className="card card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+        </div>
       </Card.Body>
     </Card>
-    );
-}
+  );
+};
 
 export default ResourceCard;
--- frontendaurora/src/resources/resources.js
+++ frontendaurora/src/resources/resources.js
@@ -4,6 +4,7 @@ import './resources.css';
 import carpentryImage from '../images/carpentry.jpg';
 import drywallImage from '../images/drywall.jpg';
 import electricalImage from '../images/electrical.jpg';
+import flooringImage from '../images/flooring.jpg';
 
 const Resources = () => {
 
@@ -13,7 +14,8 @@ const Resources = () => {
         title: 'Carpentry',
         description: 'Learn how to craft custom furniture, build shelving units, and construct wooden fixtures.',
         imageUrl: carpentryImage,
-        link: 'https://www.youtube.com/watch?v=y8W7KbJTg7A'
+        link: 'https://www.youtube.com/watch?v=y8W7KbJTg7A',
+        videoLinks: ['https://www.youtube.com/watch?v=y8W7KbJTg7A', 'https://www.youtube.com/watch?v=7VpzUYln8g4']
       },
       {
         title: 'Drywall/Plaster Work',
@@ -27,19 +29,24 @@ const Resources = () => {
         imageUrl: electricalImage,
         link: 'https://www.youtube.com/watch?v=hEDto-bnHKw'
       },
+      {
+        title: 'Flooring',
+        description: 'Safely wire lighting fixtures, outlets, and switches while troubleshooting electrical issues.',
+        imageUrl: flooringImage,
+        link: 'https://www.youtube.com/watch?v=lP7B9B7WX1E'
+      },
     ];
   
     return (
       <div className="App">
         <h1 style={{ padding: '12px'}}>Volunteer Resources</h1>
-        <div class="resource-grid">
-            <div className="card-container">
-            {cardDataArray.map((data, index) => (
-                <ResourceCard key={index} data={data} />
-            ))}
-            </div>
+        <div className="card-container">
+        {cardDataArray.map((data, index) => (
+            <ResourceCard key={index} data={data} />
+        ))}
         </div>
       </div>
+      
     );
   };
 
