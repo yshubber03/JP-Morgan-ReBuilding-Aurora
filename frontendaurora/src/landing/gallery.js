421bd72c99f189c193a7af8e07cca0d9e9c844db
Charles Harris <Charlesharris2322@gmail.com>
2023-10-07 09:23:41 -0500

    Decreased size of gallery component

--- frontendaurora/src/landing/gallery.js
+++ frontendaurora/src/landing/gallery.js
@@ -21,15 +21,17 @@ function Gallery() {
             {/* Gallery Section */}
             <section className="mt-5">
                 <h2 className="text-center mb-4">Recent Projects</h2>
-                <Carousel autoPlay infiniteLoop={true} showThumbs={false}>
+                <Carousel autoPlay infiniteLoop={true} showThumbs={false} className="custom-carousel">
         <div>
             <img
+                className="gallery-img"
                 alt="Volenteer images1"
                 src="https://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1678904790447-KUVJ42YVGHQ3FXPNRZ7X/IMG_2457.JPG?format=1000w" />
             <p className="legend">Test1</p>
         </div>
         <div>
             <img
+                className="gallery-img"
                 alt="Volenteer images2"
                 src="https://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1678905511191-DAX5Y83RS1PWDG1LAYQD/ARE+2011+volunteers+outside+home+3.jpg?format=1000w" />
 
@@ -37,6 +39,7 @@ function Gallery() {
         </div>
         <div>
             <img
+                className="gallery-img"
                 alt="Volenteer images3"
                 src="https://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1678905558994-TXIUEVMO54JK7PHPAJB8/ARE+2011+volunteers.jpg?format=1000w" />
             <p className="legend">Test3</p>
