1e984bb6c88bbb39314cfc6228586584a4b92d46
jamespomares <jamespomares@gmail.com>
2023-10-07 02:17:16 -0500

    added embedded google map test

--- frontendaurora/src/User/EventCard.js
+++ frontendaurora/src/User/EventCard.js
@@ -16,7 +16,6 @@ const EventCard = (props) => {
   }
 
   return (
-<<<<<<< HEAD
       <div className="card shadow-sm mb-3">
           <div className="card-body">
               <h5 className="card-title">{description}</h5>
@@ -26,41 +25,22 @@ const EventCard = (props) => {
 
               {/* Embedding Google Map */}
               <div className="mt-2 mb-3">
-                  <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-                      <GoogleMap
-                          center={{ lat: latitude, lng: longitude }}
-                          zoom={15}
-                          mapContainerStyle={{ width: '100%', height: '200px' }}
-                      />
-                  </LoadScript>
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95250.82546373364!2d-88.3067189!3d41.750972350000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880ee54d4ed5111b%3A0x7fd1f848c350e85d!2sAurora%2C%20IL!5e0!3m2!1sen!2sus!4v1696662104246!5m2!1sen!2sus" 
+                width="200" 
+                height="200" 
+                style={{border:0}} 
+                allowFullScreen="" 
+                loading="lazy" 
+                title="Event Location"  // Add a title for accessibility purposes
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
               </div>
               <p className="card-text"><strong>Tools Needed:</strong> {tools.join(', ')}</p>
               <button className="btn btn-danger" onClick={cancelRSVP}>Cancel</button>
           </div>
       </div>
 
-=======
-    <div className="event-card">
-      <h3>{description}</h3>
-      <p><strong>Date:</strong> {date}</p>
-      <p><strong>Time:</strong> {time}</p>
-      <p><strong>Location:</strong> {location}</p>
-      
-      <iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95250.82546373364!2d-88.3067189!3d41.750972350000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880ee54d4ed5111b%3A0x7fd1f848c350e85d!2sAurora%2C%20IL!5e0!3m2!1sen!2sus!4v1696662104246!5m2!1sen!2sus" 
-        width="200" 
-        height="200" 
-        style={{border:0}} 
-        allowFullScreen="" 
-        loading="lazy" 
-        title="Event Location"  // Add a title for accessibility purposes
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-      
-      <p><strong>Tools Needed:</strong> {tools.join(', ')}</p> {/* Convert tools array to comma-separated string */}
-      <button onClick={cancelRSVP}>Cancel</button>
-    </div>
->>>>>>> 9d0fe03 (clearing out for google map integration)
   )
 }
 
