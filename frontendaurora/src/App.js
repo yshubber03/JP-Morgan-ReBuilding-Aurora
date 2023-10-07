ebde3e913e1a484648edb4fdcf710767c74ad153
Charles Harris <Charlesharris2322@gmail.com>
2023-10-06 19:49:42 -0500

    Displays gallery in the landing page

--- frontendaurora/src/App.js
+++ frontendaurora/src/App.js
@@ -1,10 +1,14 @@
+import ImageSlider from './landing/ImageSlider'
+import {SliderData} from "./landing/gallery";
 import calender from './landing/calender.js';
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+        <ImageSlider slides={SliderData} />
+
     </div>
   );
 }
 
-export default calender;
+export default App;
