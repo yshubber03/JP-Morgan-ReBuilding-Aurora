224e0bde65ccc649866811e91d04404188463daf
jamespomares <jamespomares@gmail.com>
2023-10-07 02:08:26 -0500

    add google map embed functionality

--- frontendaurora/src/User/EventCard.js
+++ frontendaurora/src/User/EventCard.js
@@ -7,8 +7,6 @@ const EventCard = (props) => {
     date, 
     time, 
     location,
-    latitude,  // New prop for the latitude of the event's location
-    longitude, // New prop for the longitude of the event's location
     tools 
   } = props;
 
@@ -18,6 +16,7 @@ const EventCard = (props) => {
   }
 
   return (
+<<<<<<< HEAD
       <div className="card shadow-sm mb-3">
           <div className="card-body">
               <h5 className="card-title">{description}</h5>
@@ -40,6 +39,28 @@ const EventCard = (props) => {
           </div>
       </div>
 
+=======
+    <div className="event-card">
+      <h3>{description}</h3>
+      <p><strong>Date:</strong> {date}</p>
+      <p><strong>Time:</strong> {time}</p>
+      <p><strong>Location:</strong> {location}</p>
+      
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95250.82546373364!2d-88.3067189!3d41.750972350000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880ee54d4ed5111b%3A0x7fd1f848c350e85d!2sAurora%2C%20IL!5e0!3m2!1sen!2sus!4v1696662104246!5m2!1sen!2sus" 
+        width="200" 
+        height="200" 
+        style={{border:0}} 
+        allowFullScreen="" 
+        loading="lazy" 
+        title="Event Location"  // Add a title for accessibility purposes
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      
+      <p><strong>Tools Needed:</strong> {tools.join(', ')}</p> {/* Convert tools array to comma-separated string */}
+      <button onClick={cancelRSVP}>Cancel</button>
+    </div>
+>>>>>>> 9d0fe03 (clearing out for google map integration)
   )
 }
 
